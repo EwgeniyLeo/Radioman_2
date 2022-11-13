471638be5ff6a07797d174d9be4e84e80ae5bac0
Ewgeniy Leontev <esleontev@mail.ru>
2022-11-13 12:11:57 +0300

    Modify Radio Code

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,15 +1,29 @@
 package ru.netology;
 
 public class Radio {
+    private int radioStationAmount;
     private int currentRadioStation;
     private int maxRadioStation = 9;
     private int minRadioStation = 0;
     private int currentSoundVolume;
-    private int maxSoundVolume = 10;
+    private int maxSoundVolume = 100;
     private int minSoundVolume = 0;
+
+    public Radio(int radioStationAmount) {
+        this.radioStationAmount = radioStationAmount;
+        this.maxRadioStation = radioStationAmount - 1;
+    }
+
+    public Radio() {
+
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
+    }
+
+    public int getMaxRadioStation() {
+        return maxRadioStation;
     }
 
     public void setCurrentRadioStation(int currentRadioStation) {
